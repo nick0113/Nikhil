b27261dad833cdc81f70d80998d36f794c7ec067
abhi <abhi@abhijet>
2023-02-16 15:50:48 +0530

    new changes done

---
+++ NewGitHub143/src/Velocity/Test2.java
@@ -0,0 +1,28 @@
+package Velocity;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		
+		
+		String a = new String("jayesh");
+		String b = "jayesh";
+
+		System.out.println(a.toUpperCase());   //  toUpperCase for convert string is capital
+		System.out.println(b.toLowerCase());   // toLowerCas for convert string is samll
+		
+		System.out.println(b.concat(a));   //  concat for combine two String
+		System.out.println(a.length());      //  length For Calculate The string size
+		
+		
+		
+	   
+	    
+	    
+	    
+		}
+	}
+	
+	
+
